635032a433de18080dfc28446fcf759a70953a23
spantheslayer <supriyosarkar282@gmail.com>
2023-04-10 15:26:53 +0530

    added home init paid
    Signed-off-by: spantheslayer <supriyosarkar282@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,8 @@
 import React from 'react';
-import { SafeAreaView, StatusBar, Text, View, useColorScheme } from 'react-native';
+import Routing from './routes';
 
 function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  const backgroundStyle = 'bg-gray-100 dark:bg-gray-800 h-full flex items-center justify-center';
-
-  return (
-    <SafeAreaView className={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <View className="flex h-full items-center justify-center">
-        <Text className="text-white text-2xl font-bold">Hello There !!</Text>
-      </View>
-    </SafeAreaView>
-  );
+  return <Routing />;
 }
 
 export default App;
